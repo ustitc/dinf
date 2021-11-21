ded65efb2b77eb232d3d57302ebdef49e4263b0d
Ruslan Ustits <ustitsruslan@gmail.com>
2021-11-22 02:08:54 +0300

    Added modal overlay for dice creation

--- web/src/jsMain/kotlin/dinf/web/Main.kt
+++ web/src/jsMain/kotlin/dinf/web/Main.kt
@@ -1,10 +1,12 @@
 package dinf.web
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import dinf.api.APIDices
 import dinf.domain.Dices
-import org.jetbrains.compose.web.css.CSSNumeric
 import org.jetbrains.compose.web.css.height
-import org.jetbrains.compose.web.css.maxHeight
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
@@ -13,6 +15,8 @@ private val dices: Dices = APIDices("http://localhost:8080")
 
 fun main() {
     renderComposable(rootElementId = "root") {
+        var createForm by remember { mutableStateOf(false) }
+
         Nav(attrs = { classes("level") }) {
             Div(attrs = { classes("level-left") }) {
                 Div(attrs = { classes("level-item", "has-text-centered") }) {
@@ -21,6 +25,35 @@ fun main() {
                             height(50.px)
                         }
                     })
+                }
+                Div(attrs = { classes("level-item", "has-text-centered") }) {
+                    A(attrs = {
+                        classes("link", "is-info")
+                        onClick {
+                            createForm = true
+                        }
+                    }) {
+                        Text("Create dice")
+                    }
+                }
+                if (createForm) {
+                    Div(attrs = { classes("modal", "is-active") }) {
+                        Div(attrs = {
+                            classes("modal-background")
+                            onClick {
+                                createForm = false
+                            }
+                        }) { }
+                        Div(attrs = { classes("modal-content") }) {
+                            P { Text("test") }
+                        }
+                        Button(attrs = {
+                            classes("modal-close")
+                            onClick {
+                                createForm = false
+                            }
+                        }) { }
+                    }
                 }
             }
         }
