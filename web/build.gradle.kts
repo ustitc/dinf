a4209c1474d03c81262764e1e780f1a8af50bdb6
Ruslan Ustits <ustitsruslan@gmail.com>
2021-11-29 01:44:32 +0300

    Update jb-compose version

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.compose.*
 
 plugins {
     kotlin("multiplatform")
-    id("org.jetbrains.compose") version "1.0.0-beta5"
+    id("org.jetbrains.compose") version "1.0.0-rc5"
 }
 
 rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
