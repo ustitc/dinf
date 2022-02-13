e364dff9df1359e34c4081e364a709746db7aebc
Ruslan Ustits <ustitsruslan@gmail.com>
2022-02-13 16:14:31 +0300

    Fixed tests

--- backend/src/main/kotlin/dinf/domain/DiceSave.kt
+++ backend/src/main/kotlin/dinf/domain/DiceSave.kt
@@ -4,4 +4,12 @@ interface DiceSave {
 
     suspend fun create(dice: Dice): Dice
 
+    class Stub(private val list: MutableList<Dice> = mutableListOf()) : DiceSave {
+
+        override suspend fun create(dice: Dice): Dice {
+            list.add(dice)
+            return dice
+        }
+    }
+
 }
--- backend/src/main/kotlin/dinf/domain/DiceSearch.kt
+++ backend/src/main/kotlin/dinf/domain/DiceSearch.kt
@@ -7,7 +7,7 @@ interface DiceSearch {
 
     suspend fun forText(text: String): List<Dice>
 
-    class Stub(private val dices: Dices) : DiceSearch {
+    class Stub(private val dices: Dices = Dices.Stub()) : DiceSearch {
 
         override suspend fun forText(text: String): List<Dice> {
             return dices.flow().filter { it.name.nbString.contains(text) }.toList()
