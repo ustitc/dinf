068fe5881b39522314924b6e0079c2de5469c2d0
Ruslan Ustits <ustitsruslan@gmail.com>
2022-04-25 00:00:06 +0300

    Fix deprecation

--- backend/src/test/kotlin/dinf/ApplicationTest.kt
+++ backend/src/test/kotlin/dinf/ApplicationTest.kt
@@ -2,23 +2,24 @@ package dinf
 
 import dinf.config.Configuration
 import dinf.plugins.configureRouting
-import io.kotest.assertions.ktor.shouldHaveStatus
+import io.kotest.assertions.ktor.client.shouldHaveStatus
 import io.kotest.core.spec.style.StringSpec
+import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 
 class ApplicationTest : StringSpec({
 
     "root" {
-        withTestApplication({
-            configureRouting(
-                Configuration(),
-                AppDeps.Stub(),
-            )
-        }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                response shouldHaveStatus HttpStatusCode.OK
+        testApplication {
+            application {
+                configureRouting(
+                    Configuration(),
+                    AppDeps.Stub(),
+                )
             }
+            val response = client.get("/")
+            response shouldHaveStatus HttpStatusCode.OK
         }
     }
 
